707eacb5adad580ba0658c853b51d5ea6192b644
hd-script <35713255+hd-script@users.noreply.github.com>
2019-04-09 00:45:55 +0530

    Update ram-check.ps1

--- ram-check.ps1
+++ ram-check.ps1
@@ -9,6 +9,10 @@ $requiredFree = 32 # Minimum RAM set to 32GB
 if($freePhysicalMem -lt $requiredFree)
 {
     Write-Host "Out of ram"
+    unregister-ScheduledJob ramchk #removes the schedule check and postpond it run after 7 days
+    $startdt = (Get-Date).AddDays(7)
+    $triggerram = New-JobTrigger -Once -At $startdt -RepetitionInterval (New-TimeSpan -Hours 12) -RepetitionDuration ([timespan]::MaxValue)
+    Register-ScheduledJob -Name ramchk -Trigger $triggerram -FilePath C:\scripts\ram-new.ps1
     $msg.Subject = "RAM Warning: $env:COMPUTERNAME" #getting Computer name
     $msg.Body = "Hello, `nThe server $env:COMPUTERNAME is running out of RAM. Available RAM is $freePhysicalMem GB."
     $smtp.Send($msg) 
